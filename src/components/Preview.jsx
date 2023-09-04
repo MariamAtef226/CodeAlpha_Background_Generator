3448cf6081d3d2cf6aa6e52425a900512c4bd391
unknown <mariamatef226@gmail.com>
2023-09-05 02:08:44 +0300

    fixed colors display bug

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -6,7 +6,7 @@ export default function Preview({ type, normalColor, dir, grad }) {
     function copier() {
         var copyRule = document.querySelector(".color");
         navigator.clipboard
-            .writeText(copyRule.innerHTML)
+            .writeText(copyRule.innerText)
             .then(() => {
                 alert("Copied the rule successfully!");
             })
@@ -19,8 +19,8 @@ export default function Preview({ type, normalColor, dir, grad }) {
     return <div className='preview-container text-center' style={style}>
 
         <div className='color bg-light'>
-            {type == 'normal' && " background-color:{normalColor}; "}
-            {type == 'gradient' && " background-image:linear-gradient(to {dir},{grad.c1},{grad.c2});"}
+            {type == 'normal' && " background-color:"+{normalColor}+";"}
+            {type == 'gradient' && `background-image:linear-gradient(to ${dir},${grad.c1},${grad.c2});`}
         </div>
         <button className='copy' onClick={copier}>Copy Rule</button>
 
