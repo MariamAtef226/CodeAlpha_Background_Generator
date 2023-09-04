9bd4f02a98270ca3c1817c210c26f53d1d21cebc
unknown <mariamatef226@gmail.com>
2023-09-05 02:03:52 +0300

    copying to clipboard is fixed

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -5,13 +5,19 @@ export default function Preview({ type, normalColor, dir, grad }) {
     // a function to copy the resulted rule
     function copier() {
         var copyRule = document.querySelector(".color");
-        navigator.clipboard.writeText(copyRule.innerHTML);
-        alert("Copied the rule successfully!");
+        navigator.clipboard
+            .writeText(copyRule.innerHTML)
+            .then(() => {
+                alert("Copied the rule successfully!");
+            })
+            .catch(() => {
+                alert("something went wrong");
+            });
     }
 
 
     return <div className='preview-container text-center' style={style}>
-        
+
         <div className='color bg-light'>
             {type == 'normal' && " background-color:{normalColor}; "}
             {type == 'gradient' && " background-image:linear-gradient(to {dir},{grad.c1},{grad.c2});"}
